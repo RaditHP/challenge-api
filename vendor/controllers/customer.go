9ac4d5f84c2f18acb1baf2a3952c333724c8c942
RaditHP <rpramudito@gmail.com>
2022-07-14 20:40:22 +0700

    POST to create customer

--- vendor/controllers/customer.go
+++ vendor/controllers/customer.go
@@ -2,12 +2,22 @@ package controllers
 
 import (
 	"models"
+	"time"
 
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
+
+type CreateCustomerInput struct {
+	Name          string `json:"customername"`
+	ContactNumber string `json:"customeraddress"`
+	Address       string `json:"customercontno"`
+	TotalBuy      string `json:"totalbuy"`
+	CreatorID     string `json:"creatorid"`
+	Date          string `json:"date"`
+}
 
 //GET /customer gets all customers in the table
 func ListCustomers(c *gin.Context) {
@@ -18,6 +28,7 @@ func ListCustomers(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": customers})
 }
 
+// Get one customer with particular ID
 func FindCustomer(c *gin.Context) {
 	var customer models.Customer
 
@@ -27,5 +38,24 @@ func FindCustomer(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
+	c.JSON(http.StatusOK, gin.H{"data": customer})
+}
+
+func CreateCustomer(c *gin.Context) {
+	var input CreateCustomerInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	currentDate := time.Now().Format("19-10-2021")
+	date, _ := time.Parse(currentDate, input.Date)
+
+	//create customer
+	customer := models.Customer{Name: input.Name, ContactNumber: input.ContactNumber,
+		Address: input.Address, TotalBuy: input.TotalBuy, CreatorID: input.CreatorID, Date: date}
+
+	db := c.MustGet("db").(*gorm.DB)
+	db.Create(&customer)
+
 	c.JSON(http.StatusOK, gin.H{"data": customer})
 }
