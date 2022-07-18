9d62b0e4d8de7f3d70835687135fbdf4eeb90777
RaditHP <rpramudito@gmail.com>
2022-07-18 13:34:12 +0700

    Update sell endpoint using PATCH

--- vendor/routes/routes.go
+++ vendor/routes/routes.go
@@ -15,13 +15,14 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 	route.GET("/customers", controllers.ListCustomers)
 	route.GET("/customers/:id", controllers.FindCustomer)
 	route.POST("/customers", controllers.CreateCustomer)
-	route.PATCH("customers/:id", controllers.UpdateCustomer)
+	route.PATCH("/customers/:id", controllers.UpdateCustomer)
 	route.DELETE("/customers/:id", controllers.DeleteCustomer)
 
 	//route for Sell table
 	route.GET("/sell", controllers.ListSell)
 	route.GET("/sell/:id", controllers.GetSell)
 	route.POST("/sell", controllers.CreateSell)
+	route.PATCH("/sell/:id", controllers.UpdateSell)
 	route.DELETE("/sell/:id", controllers.DeleteSell)
 	return route
 }
